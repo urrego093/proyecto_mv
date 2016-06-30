673e3c888500d8de9fbdc5a2a91b0a781b977b63
urrego093 <urrego@outlook.com>
2016-06-30 17:14:21 -0500

    Se pueden manejar servicios del sistema, muestran los puertos abiertos, algunas tareas ahora muestran cierta salida, se corrigieron algunos errores para enviar archivos y se hicieron algunos ajustes a la interfaz

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$acc4f3d201d225a5$a1746104369e57ac3642f5a81d79dbdb2db87fad"
-=======
-password="pbkdf2(1000,20,sha512)$9fa40bc934582f53$5ab622dbcc98aeff844e085bd8252fa84d24edf2"
->>>>>>> master
+password="pbkdf2(1000,20,sha512)$8b1e7c7aa427a5a5$7ff8b1dee0870b120791de56dff66d2749266e95"
