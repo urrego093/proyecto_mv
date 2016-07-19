e2dd7a45b5f542babf5c305219594f372ae03367
root <root@localhost.localdomain>
2016-07-18 22:53:59 -0500

    reg machine

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$acc4f3d201d225a5$a1746104369e57ac3642f5a81d79dbdb2db87fad"
-=======
-password="pbkdf2(1000,20,sha512)$9fa40bc934582f53$5ab622dbcc98aeff844e085bd8252fa84d24edf2"
->>>>>>> master
+password="pbkdf2(1000,20,sha512)$8635601e1b857268$bc8df621454c11cbeda8a38f0cce42a0986392e0"
