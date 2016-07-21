4feee976359e71536322530c77f3af3fee0c7d35
root <root@localhost.localdomain>
2016-07-21 12:07:57 -0500

    modificado el moduilo apra crear usuarios, ahora tiene compatibilidad con distribuciones Debian y RH

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$acc4f3d201d225a5$a1746104369e57ac3642f5a81d79dbdb2db87fad"
-=======
-password="pbkdf2(1000,20,sha512)$9fa40bc934582f53$5ab622dbcc98aeff844e085bd8252fa84d24edf2"
->>>>>>> master
+password="pbkdf2(1000,20,sha512)$bb568fc670750941$964207569f8923d3e42d8a92a05f6564a9defefb"
